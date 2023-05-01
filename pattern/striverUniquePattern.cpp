35503199a1dd00660ed1292122b220bb7895edb3
dharmik-1611 <rathoddharmik1990@gmail.com>
2023-05-01 08:28:54 +0530

    striver's pattern program solving

---
+++ pattern/striverUniquePattern.cpp
@@ -0,0 +1,455 @@
+
+// p-1
+//  #include <bits/stdc++.h>
+//  using namespace std;
+//  int main()
+//  {
+//      int n;
+//      cin >> n;
+//      for (int i = 1; i <= n; i++)
+//      {
+//          for (int j = 1; j <= (n - i + 1); j++)
+//          {
+//              cout << "*";
+//          }
+//          cout << endl;
+//      }
+//  }
+
+// p-2
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= (n - i + 1); j++)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-3
+// logic space star space
+// logic [n-i+1,2*i+1,n-i+1]
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         //space
+//         for (int j = 0; j < (n - i - 1); j++)
+//         {
+//             cout << " ";
+//         }
+//         //stars
+//         for (int j = 0; j < 2 * i + 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         //space
+//         for (int j = 0; j < (n - i - 1); j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-4
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         // space
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+//         {
+//             cout << "*";
+//         }
+//         // space
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-5 is combination of pattern 3 and pattern 4
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         // space
+//         for (int j = 0; j < (n - i - 1); j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 0; j < 2 * i + 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         // space
+//         for (int j = 0; j < (n - i - 1); j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         // space
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+//         {
+//             cout << "*";
+//         }
+//         // space
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p - 6
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= 2 * n - 1; i++)
+//     {
+//         int stars = i;
+//         if (i > n)
+//             stars = 2 * n - i;
+//         for (int j = 1; j <= stars; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p -7
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             if ((i + j) % 2 == 0)
+//             {
+//                 cout << 1 << " ";
+//             }
+//             else
+//             {
+//                 cout << 0 << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-8
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int space = 2 * (n - 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         // numbers
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << j;
+//         }
+//         // spaces
+//         for (int j = 1; j <= space; j++)
+//         {
+//             cout << " ";
+//         }
+//         // numbers
+//         for (int j = i; j >= 1; j--)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//         space -= 2;
+//     }
+// }
+
+// p -9
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         char ch = 'A';
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << ch;
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-10
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         char ch = 'A';
+//         for (int j = 1; j <= (n - i + 1); j++)
+//         {
+//             cout << ch;
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-11
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     char ch = 'A';
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << ch;
+//         }
+//         ch++;
+//         cout << endl;
+//     }
+// }
+
+// p-12
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         // space
+//         for (int j = 1; j <= n - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         // characters
+//         char ch = 'A';
+//         int breakPoint = (2 * i + 1) / 2;
+//         for (int j = 1; j <= 2 * i + 1; j++)
+//         {
+//             cout << ch;
+//             if (j <= breakPoint)
+//                 ch++;
+//             else
+//                 ch--;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-13
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         char start = 'A' + n - i - 1;
+//         for (int j = 0; j <= i; j++)
+//         {
+//             cout << start;
+//             start++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p-14
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int space = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         // stars
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "*";
+//         }
+//         // spaces
+//         for (int j = 1; j <= space; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "*";
+//         }
+//         space += 2;
+//         cout << endl;
+//     }
+//     int space2 = 2 * n - 2;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         // stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "*";
+//         }
+//         // spaces
+//         for (int j = 1; j <= space2; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "*";
+//         }
+//         space2 -= 2;
+//         cout << endl;
+//     }
+// }
+
+// p-15
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int space = 2 * n - 2;
+//     for (int i = 1; i <= 2 * n - 1; i++)
+//     {
+//         int stars = i;
+//         if (i > n)
+//             stars = 2 * n - i;
+
+//         // stars
+//         for (int j = 1; j <= stars; j++)
+//         {
+//             cout << "*";
+//         }
+
+//         // spaces
+
+//         for (int j = 1; j <= space; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 1; j <= stars; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//         if (i < n)
+//             space -= 2;
+//         else
+//             space += 2;
+//     }
+// }
+
+// p-16
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (i == 1 || i == n || j == 1 || j == n)
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// p -17
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < 2 * n - 1; i++)
+//     {
+//         for (int j = 0; j < 2 * n - 1; j++)
+//         {
+//             int top = i;
+//             int left = j;
+//             int right = (2 * n - 2) - j;
+//             int down = (2 * n - 2) - i;
+//             cout << (n - min(min(top, down), min(left, right)));
+//         }
+//         cout << endl;
+//     }
+// }
